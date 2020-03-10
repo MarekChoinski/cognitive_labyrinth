042774198d3302ff564d32e758863b3b78975689
Marek Choiński <marekchoinski17@gmail.com>
2020-03-10 17:33:02 +0100

    Installing openCV

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,3 +1,18 @@
 import '../styles/index.scss';
 
-console.log('webpack starterkit');
+let video = document.getElementById("videoInput"); // video is the id of video tag
+navigator.mediaDevices.getUserMedia({ video: true, audio: false })
+    .then(function (stream) {
+        video.srcObject = stream;
+        video.play();
+    })
+    .catch(function (err) {
+        console.log("An error occurred! " + err);
+    });
+
+console.log("xd");
+
+let openCV = document.getElementById('opencv');
+openCV.onload = () => {
+    document.getElementById('status').innerHTML = 'OpenCV.js is ready.';
+};
