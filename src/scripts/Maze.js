e2826a9364ceef6a4811495a6853ee32b4ba9aca
Marek Choiński <marekchoinski17@gmail.com>
2020-03-12 19:55:26 +0100

    Fetching one frame and working on it

--- src/scripts/Maze.js
+++ src/scripts/Maze.js
@@ -1,31 +1,67 @@
 export default class Maze {
+
+
     constructor(video) {
         // this.video = video;
-        this.src = new cv.Mat(video.height, video.width, cv.CV_8UC4);
+        this.frame_from_video = new cv.Mat(video.height, video.width, cv.CV_8UC4);
         this.dst = new cv.Mat(video.height, video.width, cv.CV_8UC1);
+        this.labirynth_mask = new cv.Mat(video.height, video.width, cv.CV_8UC1);
         this.cap = new cv.VideoCapture(video);
 
         this.FPS = 20;
+        this.sensivity_of_geeting_labirynth = 90;
+
+        this.circles = new Array(video.height).fill().map(() => new Array(video.width).fill([0, 0, 0]));
+        //this.labirynth_mask = new Array(video.height).fill().map(() => new Array(video.width).fill([0, 0, 0]));
+        this.circles_mask = new Array(video.height).fill().map(() => new Array(video.width).fill([0, 0, 0]));
+
+        this.lower_green = [40, 100, 85];
+        this.upper_green = [75, 255, 255];
+
     }
 
-    start() {
-
-
-        // schedule the first one.
-        processVideo();
-    }
-
-    processVideo() {
+    showVideo() {
         try {
-            let begin = Date.now();
-            this.cap.read(this.src);
-            cv.cvtColor(this.src, this.dst, cv.COLOR_RGBA2GRAY);
-            cv.imshow('canvas_output', this.dst);
+            // get camera_frame
+            this.cap.read(this.frame_from_video);
 
-            // TODO i don't like this fetch Date.now. It is optimal?
-            setTimeout(processVideo, 1000 / this.FPS - (Date.now() - begin));
+            let dst = this.frame_from_video;
+
+
+
+            cv.imshow('canvas_output', dst);
         } catch (err) {
             console.log(err);
         }
-    };
+    }
+
+    // we should find position of end points
+    // there could be a lot of green elements on frame
+    // so we should find two the biggest
+    // returns position of start and end and diameter of found point
+    find_position_of_end_points() {
+
+    }
+
+    calculateMaze() {
+        let gray = new cv.Mat();
+        cv.cvtColor(this.frame_from_video, gray, cv.COLOR_RGBA2GRAY, 0);
+        cv.threshold(gray, 255, this.labirynth_mask, this.sensivity_of_geeting_labirynth, cv.THRESH_BINARY);
+
+
+    }
+
+    start() {
+        setInterval(() => this.showVideo(), 1000 / this.FPS);
+        setInterval(() => this.calculateMaze(), 0);
+    }
+
+    //todo
+    // free(){
+    //     clearInterval(refreshIntervalId);
+    //     clearInterval(refreshIntervalId);
+    // src.delete(); dst.delete();
+    // }
+
+
 }
