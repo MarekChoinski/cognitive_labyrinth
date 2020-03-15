a53a1c20dffce05a65fe059b97f382a7e36f7fef
Marek Choiński <marekchoinski17@gmail.com>
2020-03-16 00:27:22 +0100

    Asynchronous progressing labirynth

--- src/scripts/Maze.js
+++ src/scripts/Maze.js
@@ -10,7 +10,7 @@ export default class Maze {
         // this.frame_from_video = cv.imread('canvas_input');
         this.dst = new cv.Mat(video.height, video.width, cv.CV_8UC1);
         this.labirynth_mask = new cv.Mat(video.height, video.width, cv.CV_8UC1);
-        this.hsv_frame = new cv.Mat(video.height, video.width, cv.CV_8UC4);
+        // this.hsv_frame = new cv.Mat(video.height, video.width, cv.CV_8UC4);
         // this.hsv_frame2 = new cv.Mat(video.height, video.width, cv.CV_8U);
         //this.cap = new cv.VideoCapture(video);
 
@@ -48,10 +48,12 @@ export default class Maze {
             // cv.imshow('canvas_output', this.labirynth_mask);
 
 
-            cv.cvtColor(this.frame_from_video, this.hsv_frame, cv.COLOR_BGR2HSV, 0);
+
+            // let hsv_frame = new cv.Mat();
+            // cv.cvtColor(this.frame_from_video, hsv_frame, cv.COLOR_BGR2HSV, 0);
             // this.hsv_frame = hsv;
-            let low = new cv.Mat(this.hsv_frame.rows, this.hsv_frame.cols, this.hsv_frame.type(), this.lower_green);
-            let high = new cv.Mat(this.hsv_frame.rows, this.hsv_frame.cols, this.hsv_frame.type(), this.upper_green);
+            // let low = new cv.Mat(hsv_frame.rows, hsv_frame.cols, hsv_frame.type(), this.lower_green);
+            // let high = new cv.Mat(hsv_frame.rows, hsv_frame.cols, hsv_frame.type(), this.upper_green);
 
 
             // console.log(this.frame_from_video.channels());
@@ -61,18 +63,20 @@ export default class Maze {
 
 
 
-            let inr = new cv.Mat();
-            cv.inRange(this.hsv_frame, low, high, inr);
+            // let inr = new cv.Mat();
+            // cv.inRange(hsv_frame, low, high, inr);
             // console.log(inr.channels());
             // console.log();
 
-            cv.imshow('canvas_output', inr);
+            // cv.imshow('canvas_output', inr);
+            cv.imshow('canvas_output', this.labirynth_mask);
             // dist.delete();
             // this.frame_from_video.delete();
 
-            inr.delete();
-            low.delete();
-            high.delete();
+            // inr.delete();
+            // hsv_frame.delete();
+            // low.delete();
+            // high.delete();
         } catch (err) {
             console.log(err);
         }
@@ -82,7 +86,7 @@ export default class Maze {
     // there could be a lot of green elements on frame
     // so we should find two the biggest
     // returns position of start and end and diameter of found point
-    find_position_of_end_points() {
+    find_position_of_end_points(points_mask) {
 
     }
 
@@ -95,23 +99,52 @@ export default class Maze {
             // console.log(gray);
 
             cv.cvtColor(this.frame_from_video, gray, cv.COLOR_RGBA2GRAY, 0);
-            // threshold image to get only black labirynth
-            cv.threshold(gray, this.labirynth_mask, this.sensivity_of_geeting_labirynth, 255, cv.THRESH_BINARY);
+            // threshold image to emilinate white colors - we get only black labirynth + green points
+            // let thresholded_labirynt = new cv.Mat();
+            cv.threshold(gray, gray, this.sensivity_of_geeting_labirynth, 255, cv.THRESH_BINARY_INV);
 
-            // You can try more different parameters
-            // let hsv = new cv.Mat();
-            cv.cvtColor(this.frame_from_video, this.hsv_frame, cv.COLOR_BGR2HSV, 0);
-            // this.hsv_frame = hsv;
-            let low = new cv.Mat(this.frame_from_video.rows, this.frame_from_video.cols, this.frame_from_video.type(), [0, 0, 0, 0]);
-            let high = new cv.Mat(this.frame_from_video.rows, this.frame_from_video.cols, this.frame_from_video.type(), [150, 150, 150, 255]);
+            // dilatation for bolder walls of maze
+            // const kernel = cv.Mat.ones(5, 5, cv.CV_8U);
+            // const anchor = new cv.Point(-1, -1);
+            cv.dilate(
+                gray,
+                this.labirynth_mask,
+                cv.Mat.ones(5, 5, cv.CV_8U),
+                new cv.Point(-1, -1),
+                1,
+                cv.BORDER_CONSTANT,
+                cv.morphologyDefaultBorderValue()
+            );
 
+            // we need to get green points mask for differentiation of labirynth mask
+            let hsv = new cv.Mat();
+            cv.cvtColor(this.frame_from_video, hsv, cv.COLOR_BGR2HSV, 0);
+            const low = new cv.Mat(hsv.rows, hsv.cols, hsv.type(), this.lower_green);
+            const high = new cv.Mat(hsv.rows, hsv.cols, hsv.type(), this.upper_green);
 
-            // cv.inRange(this.frame_from_video, low, high, this.hsv_frame2);
+            let points_mask = new cv.Mat();
+            cv.inRange(hsv, low, high, points_mask);
+            cv.dilate(
+                points_mask,
+                points_mask,
+                cv.Mat.ones(5, 5, cv.CV_8U),
+                new cv.Point(-1, -1),
+                1,
+                cv.BORDER_CONSTANT,
+                cv.morphologyDefaultBorderValue()
+            );
 
+            let mask = new cv.Mat();
+            // let dtype = -1;
+            cv.subtract(this.labirynth_mask, points_mask, this.labirynth_mask, mask, -1);
+
+            gray.delete();
+            mask.delete();
+            // thresholded_labirynt.delete();
             low.delete();
             high.delete();
-            gray.delete();
-            // hsv.delete();
+            hsv.delete();
+            points_mask.delete();
         } catch (error) {
             console.log(error);
         }
@@ -121,7 +154,8 @@ export default class Maze {
         this.frame_from_video = cv.imread(this.video);
         console.log(this.frame_from_video);
         setInterval(() => this.showVideo(), 1000 / this.FPS);
-        // setInterval(() => this.calculateMaze(), 0);
+
+        setInterval(() => this.calculateMaze(), 0); //TODO: probably shouldn't be faster than FPS, but idk tbh // TODO2: or make some guard variable to check if there is new frame - eg. is_new_frame = false
     }
 
     //todo
